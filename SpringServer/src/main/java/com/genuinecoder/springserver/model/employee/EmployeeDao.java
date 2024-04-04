7c7d2b74d218f9bd3316bd11ec2fc7ba4dedeb9f
Luiss Montoya <luisgoemon@gmail.com>
2024-04-04 18:20:11 -0500

    Update EmployeeDao.java

--- SpringServer/src/main/java/com/genuinecoder/springserver/model/employee/EmployeeDao.java
+++ SpringServer/src/main/java/com/genuinecoder/springserver/model/employee/EmployeeDao.java
@@ -2,6 +2,8 @@ package com.genuinecoder.springserver.model.employee;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,10 @@ public class EmployeeDao {
   public Employee save(Employee employee) {
     return repository.save(employee);
   }
+  
+   public Optional<Employee> findById(int employeeId) {
+	   return repository.findById(employeeId);
+   }
 
   public List<Employee> getAllEmployees() {
     List<Employee> employees = new ArrayList<>();
@@ -22,6 +28,20 @@ public class EmployeeDao {
         .forEach(employees::add);
     return employees;
   }
+  
+  public Employee updateEmployee(Employee employee) {
+	    Optional<Employee> existingEmployee = repository.findById(employee.getId());
+
+	    if (existingEmployee.isPresent()) {
+	      Employee updatedEmployee = existingEmployee.get();
+	      updatedEmployee.setName(employee.getName()); 
+	      updatedEmployee.setLocation(employee.getLocation());
+	      updatedEmployee.setBranch(employee.getBranch());
+	      return repository.save(updatedEmployee);
+	    } else {
+	      throw new RuntimeException("Employee with ID " + employee.getId() + " not found");
+	    }
+	  }
 
   public void delete(int employeeId) {
     repository.deleteById(employeeId);
